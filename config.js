30bbbb070f03f2567320e323ca23436bb6505045
NvQuii <nguyenvietquy001@gmail.com>
2024-09-28 22:46:13 +0700

    Add files via upload
    hi

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '(nhấn để mở ra)';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'MÓN QUÀ CỦA ANH';
+const titleCard = 'Gửi Tặng Em';
+const contentCard = 'Chúc mừng sinh nhật vợ iu của anh. Anh có một món quà dành cho em, món quà này tuy nhỏ nma nó chứa đựng tình yêu to lớn và công sức bao đêm của anh. Hãy nhấn vào ảnh của em để đến với món quà này nhé. =====>';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '1111.JPG';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
